ab951e9252503a49a13cf6782c9ea53b49a3c5aa
luckxudong <LuckXudong@gmail.com>
2013-02-22 15:28:01 +0000

    fps

--- src/com/ice/test/WorkingWithNormalWidget.java
+++ src/com/ice/test/WorkingWithNormalWidget.java
@@ -1,10 +1,11 @@
 package com.ice.test;
 
 import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-import com.ice.test.R;
-
-import java.lang.Override;
+import android.widget.RelativeLayout;
+import com.ice.common.GlslSurfaceView;
+import com.ice.test.light.diffuse_lighting.PerFragmentLighting;
 
 /**
  * User: jason
@@ -12,12 +13,32 @@ import java.lang.Override;
  */
 public class WorkingWithNormalWidget extends Activity {
 
+    private GLSurfaceView glSurfaceView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main);
+
+        glSurfaceView = new GlslSurfaceView(this);
+        glSurfaceView.setRenderer(new PerFragmentLighting.Renderer(this));
+
+        RelativeLayout rootView = (RelativeLayout) findViewById(R.id.root);
+
+        rootView.addView(glSurfaceView, 0);
     }
 
+    @Override
+    protected void onResume() {
+        glSurfaceView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        glSurfaceView.onPause();
+        super.onPause();
+    }
 
 }
