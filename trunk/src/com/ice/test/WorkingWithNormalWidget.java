70664acc65139868d9b567fac3aeee41dcd6d3a9
LuckXudong@gmail.com <LuckXudong@gmail.com@b90d9380-08d7-ffc3-d16a-dfe6cfa6d37a>
2013-02-22 10:37:22 +0000

    WorkingWithNormalWidget

---
+++ trunk/src/com/ice/test/WorkingWithNormalWidget.java
@@ -0,0 +1,23 @@
+package com.ice.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.ice.test.R;
+
+import java.lang.Override;
+
+/**
+ * User: jason
+ * Date: 13-2-22
+ */
+public class WorkingWithNormalWidget extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+    }
+
+
+}
