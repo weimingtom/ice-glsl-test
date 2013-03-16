a7854bb1c2ecc2d46ca355c53d9ab0bc72705eeb
luckxudong <LuckXudong@gmail.com>
2013-03-16 08:12:46 +0000

    prepare for shadow test-->render depth

--- assets/shadow_map/depth_vertex.glsl
+++ assets/shadow_map/depth_vertex.glsl
@@ -3,11 +3,11 @@
 uniform mat4 u_LightMVPMatrix;
 uniform mat4 u_ModelMatrix;
 
-attribute vec4 a_Position;
+attribute vec3 a_Position;
 
 void main()
 {
 
-    gl_Position = u_MVPMatrix *(u_ModelMatrix* vect4(a_Position,1.0));
+    gl_Position = u_LightMVPMatrix *(u_ModelMatrix* vec4(a_Position,1.0));
 
 }
