e72f8a1a85318e2466a1badc2d287f32a9d125f6
LuckXudong@gmail.com <LuckXudong@gmail.com@b90d9380-08d7-ffc3-d16a-dfe6cfa6d37a>
2013-03-19 02:40:11 +0000

    Exponential shadow map algorithm

--- trunk/assets/shadow_map/shadow_map_fragment.glsl
+++ trunk/assets/shadow_map/shadow_map_fragment.glsl
@@ -7,11 +7,11 @@ uniform sampler2D u_DepthMap;
 varying vec3 v_Normal;
 varying vec2 v_TexCoordinate;
 varying vec4 v_PositionInLightSpace;
-  
+
+const vec4 bitShifts = vec4(1.0,1.0 / 255.0,1.0 / (255.0 * 255.0),1.0 / (255.0 * 255.0 * 255.0));
+
 void main()
 {                              
-
-  vec3 depth = v_PositionInLightSpace.xyz/v_PositionInLightSpace.w;
 
   vec3 normalLightDir= normalize(u_LightVector);
 
@@ -19,12 +19,18 @@ void main()
 
   diffuse = diffuse + 0.1;
 
-  //gl_FragColor = diffuse * texture2D(u_DepthMap, v_TexCoordinate);
   gl_FragColor = diffuse * texture2D(u_Texture, v_TexCoordinate);
 
-  if(depth.z> texture2D(u_DepthMap, depth.xy)){
-        gl_FragColor=0.5*gl_FragColor;
-  }
+  vec3 depth = v_PositionInLightSpace.xyz/v_PositionInLightSpace.w;
+
+  // Exponential shadow map algorithm
+  float c = 4.0;
+  vec4 texel = texture2D(u_DepthMap, depth.xy);
+  float z=dot(texel, bitShifts);
+  float shadow = clamp(exp(-c * (depth.z - z)), 0.0, 1.0);
+
+  // Apply colour and shadow
+  gl_FragColor = clamp(gl_FragColor * shadow, 0.0, 1.0);
 
 }                                                                     	
 
