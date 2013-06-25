c6b1a16d99b95a9e43c99c79a501910735cc5590
LuckXudong@gmail.com <LuckXudong@gmail.com@b90d9380-08d7-ffc3-d16a-dfe6cfa6d37a>
2013-06-25 11:11:35 +0000

    任意宽高比的 camera preview

--- trunk/assets/camera_preview/fragment.glsl
+++ trunk/assets/camera_preview/fragment.glsl
@@ -3,8 +3,6 @@
 precision mediump float;
 
 uniform samplerExternalOES u_Texture;
-
-uniform mat4 u_TextureMatrix;
 uniform vec2 u_TextureScale;
 
 varying vec2 v_TexCoordinate;
@@ -12,10 +10,6 @@ varying vec2 v_TexCoordinate;
 // The entry point for our fragment shader.
 void main()                    		
 {
-    //vec4 texCoord=u_TextureMatrix * vec4(v_TexCoordinate,1,1);
-    //vec2 coord=vec2(texCoord.x,texCoord.y);
-    //gl_FragColor = texture2D(u_Texture, coord);
-
      vec2 coord=vec2(v_TexCoordinate.x*u_TextureScale.x,v_TexCoordinate.y*u_TextureScale.y);
      gl_FragColor = texture2D(u_Texture, coord);
 }
